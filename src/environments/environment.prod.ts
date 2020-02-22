73e7a8d5707cbe628c57cddcdac51ceb8beaf60f
Jimmy Liu <dllyj86@gmail.com>
2020-02-22 18:51:18 +0800

    Change host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_HOST: 'http://localhost:8888',
+  API_HOST: 'http://52.76.30.89/:8888',
   CITY_LIST_API: '/api/weather/citylist',
   CURRENT_WEATHER_API: '/api/weather/current'
 };
